1632e1a1abff380e8f8486c0f7729122294511b7
Colin Macdonald <macdonald@maths.ox.ac.uk>
2011-07-21 16:17:43 +0000

    small updates, should've been with last night's commit

--- surfaces/cpHemisphere.m
+++ surfaces/cpHemisphere.m
@@ -1,4 +1,4 @@
-function [cpx,cpy,cpz, dist, bdy] = cpHemisphere(x,y,z, R, xc,yc,zc)
+function [cpx,cpy,cpz, dist, bdy] = cpHemisphere(x,y,z, R, cen)
 %CPHEMISPHERE  Closest point function for a hemisphere.
 %   The hemisphere consists of those points with z >= 0.
 %   [cpx,cpy,cpz, dist, bdy] = cpHemisphere(x,y,z)
@@ -6,8 +6,8 @@ function [cpx,cpy,cpz, dist, bdy] = cpHemisphere(x,y,z, R, xc,yc,zc)
 %      "bdy" is non-zero for points on the boundary.
 %   [cpx,cpy,cpz, dist, bdy] = cpHemisphere(x,y,z, R)
 %      A radius R hemisphere centered at the origin.
-%   [cpx,cpy,cpz, dist, bdy] = cpHemisphere(x,y,z, R, xc,yc,zc)
-%      A radius R hemisphere centered at (xc,yc,zc).
+%   [cpx,cpy,cpz, dist, bdy] = cpHemisphere(x,y,z, R, CEN)
+%      A radius R hemisphere centered at CEN.
 %
 %   Code is vectorized: any size/shape for x should work.
 
@@ -16,22 +16,20 @@ function [cpx,cpy,cpz, dist, bdy] = cpHemisphere(x,y,z, R, xc,yc,zc)
   if (nargin < 4)
     R = 1;
   end
-  if (nargin == 5) | (nargin == 6)
-    error('must specify all of (xc,yc,zc)');
-  end
   % default center is the origin
-  if (nargin < 7)
-    xc = 0; yc = 0; zc = 0;
+  if (nargin < 5)
+    cen = [0,0,0];
   end
 
   % shift to the origin
-  x = x - xc;
-  y = y - yc;
-  z = z - zc;
+  x = x - cen(1);
+  y = y - cen(2);
+  z = z - cen(3);
+
 
   [cpx, cpy, cpz] = cpSphere(x, y, z, R);
 
-  % should not include the bdy itself (hence not z <= 0)
+  % should not include the bdy itself (hence <=)
   bdy = (z < 0);
 
   % points with z < 0 map to the z=0 plane circle of radius R
@@ -48,6 +46,6 @@ function [cpx,cpy,cpz, dist, bdy] = cpHemisphere(x,y,z, R, xc,yc,zc)
   dist = sqrt( (x-cpx).^2 + (y-cpy).^2 + (z-cpz).^2 );
 
   % shift back to center
-  cpx = cpx + xc;
-  cpy = cpy + yc;
-  cpz = cpz + zc;
+  cpx = cpx + cen(1);
+  cpy = cpy + cen(2);
+  cpz = cpz + cen(3);
