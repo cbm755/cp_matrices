7b0fede3ce1689824d95c128911226f914624b13
Colin Macdonald <macdonald@maths.ox.ac.uk>
2011-09-29 15:28:39 +0000

    playing with finite differences and reinit

--- cp_matrices/porcupine_plot2d.m
+++ cp_matrices/porcupine_plot2d.m
@@ -1,15 +1,30 @@
-function porcupine_plot(x, y, cpx, cpy, fignum)
+function porcupine_plot2d(x, y, cpx, cpy, fignum, bdy)
+%PORCUPINE_PLOT2D  A plot of closest points
+%   porcupine_plot2d(x, y, cpx, cpy, fignum, bdy)
 
   if nargin < 5
     fignum = figure();
+  end
+  if nargin < 6
+    bdy = zeros(size(cpx));
   end
 
   figure(fignum); clf; hold on;
   xlabel('x'); ylabel('y');
 
   for i=1:length(cpx(:))
-    plot([x(i) cpx(i)], [y(i) cpy(i)], 'k-');
-    plot(cpx(i),cpy(i), 'r*');
+    if (bdy(i) == 0)
+      col = 'k';
+    elseif (bdy(i) == 1)
+      col = 'r';
+    elseif (bdy(i) == 2)
+      col = 'b';
+    else
+      col = 'y';
+    end
+    plot([x(i) cpx(i)], [y(i) cpy(i)], [col '-']);
+    plot(cpx(i),cpy(i), [col '*']);
   end
 
   axis equal
+
