94431381e3f9670fe3eedf1e1f7014ffb45b9ba8
Colin Macdonald <macdonald@maths.ox.ac.uk>
2011-10-07 16:41:22 +0000

    small comment changes

--- surfaces/paramCircle.m
+++ surfaces/paramCircle.m
@@ -1,13 +1,19 @@
-function [x,y,th] = paramCircle(n, R, cen)
+function [x, y, th] = paramCircle(n, R, cen)
 %PARAMCIRCLE   A parameterization of a circle
+%   [x, y] = paramCircle(n, R, cen)
+%   [x, y, th] = paramCircle(n, R, cen)
+%   'n' specifies the approximate number of points.
+%   'R' and 'cen' are optional.
 
   % defaults
   if (nargin < 2)
     R = 1;
   end
   if (nargin < 3)
-    cen = [0,0];
+    cen = [0 0];
   end
+
+  n = max(n, 3);
 
   th = ( 0:(2*pi/n):(2*pi) )';
 
