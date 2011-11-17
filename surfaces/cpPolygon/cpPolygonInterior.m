c7d2058f5c6b8c4290b9c8e17d0a27dfb131f03b
Colin Macdonald <macdonald@maths.ox.ac.uk>
2011-11-17 22:03:22 +0000

    Modify tom's cpPoly code to do solid polygon's (codim 0).  Start a "crystal skylight" code

---
+++ surfaces/cpPolygon/cpPolygonInterior.m
@@ -0,0 +1,30 @@
+function [cpx, cpy, dist, bdy] = cpPolygonInterior(x, y, poly)
+%CPPOLYGON  Closest Point function for a polgyon
+%   The polygon does not need to be convex.
+%
+%   [cpx, cpy] = cpPolygon(x, y, poly)
+%   [cpx, cpy, sdist] = cpPolygon(x, y, poly)
+%
+%   You can generate 'poly' interactively by calling
+%
+%      poly = makePolyInteractive()
+%
+%   If you leave off poly it will use a default one.
+%
+%   Note: returns signed distance (with negative inside).
+
+  % defaults
+  if (nargin < 3)
+    poly = default_poly();
+  end
+
+  [cpx,cpy] = helper_CPopPoly(x, y, poly);
+  sdist = helper_sDistPoly(x, y, poly);
+  sdist = -sdist;
+
+  bdy = (sdist > 0);
+  
+  cpx = (sdist > 0) .* cpx  +  (sdist <= 0) .* x;
+  cpy = (sdist > 0) .* cpy  +  (sdist <= 0) .* y;
+  
+  dist = (sdist >= 0) .* sdist;
