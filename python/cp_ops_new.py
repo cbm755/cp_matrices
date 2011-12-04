98e634d6e72556d9c3244dc4fc182837d272b7ef
Colin Macdonald <macdonald@maths.ox.ac.uk>
2011-12-04 15:16:24 +0000

    more python, should've been with prev-to-prev commit

--- python/cp_ops_new.py
+++ python/cp_ops_new.py
@@ -1,32 +1,190 @@
-def buildDiffMatrix(g, Levolve, Lextend):
+import stencils
+from numpy import array as a
+from cpOps import findGridInterpBasePt, buildInterpWeights
+
+def buildDiffMatrix(g):
     """
     generate the matrix D
+    TODO: right now defaults to Laplacian but this should be a general
+    function
     """
     from math import log10,ceil
     from scipy.sparse import coo_matrix
     from time import time
 
-    progout = 10 ** (ceil(log10(len(Levolve)))-1)
+
+    progout = 10 ** (ceil(log10(len(g.band)))-1)
     print "building D"
     st=time()
 
-    dx = Levolve[0].dx
-    diffWeights = Levolve[0].root.DiffWeights(dx)
+    # get the stencil info
+    (f,stencil,armlen) = stencils.Laplacian_2nd(dim=2)
+    diffWeights = f(g.dx)
     stencilsize = len(diffWeights)
-    #D = lil_matrix( (len(Levolve),len(Lextend)), dtype=type(dx) )
+
     # make empty lists for i,j and a_{ij}
     ii = [];  jj = [];  aij = []
     #for i,n in enumerate(Levolve):
-    for i in range(0, len(g.band)):
-        if i % progout == 0:  print "  D row " + str(i)
-        ## Slow approach using lil_matrix
-        #for s,j in enumerate(n.diffpts):
-        #    D[i,j] = diffWeights[s]
-        ## Fast coo_matrix approach
-        ii.extend([i]*stencilsize)
-        jj.extend(n.diffpts)
-        aij.extend(diffWeights)
-    D = coo_matrix( (aij,(ii,jj)), shape=(len(Levolve),len(Lextend)), dtype=type(dx) )
-    print "  D row " + str(len(Levolve))
+    for c in range(0, len(g.band)):
+        if c % progout == 0:  print "  D row " + str(c)
+        ij = g.ij[c:c+1]
+        ii.extend([c]*stencilsize)
+        # no banding:
+        #ii.extend([g.band[c]]*stencilsize)
+        for s,pt in enumerate(stencil):
+            ij2 = ij + pt
+            # TODO, clean up
+            if (ij2[0][0] < 0):
+                print('warning: periodic BC applied on x left')
+                ij2[0][0] = g.nx - 1
+            if (ij2[0][1] < 0):
+                print('warning: periodic BC applied on y left')
+                ij2[0][1] = g.ny - 1
+            if (ij2[0][0] >= g.nx):
+                print('warning: periodic BC applied on x right')
+                ij2[0][0] = 0
+            if (ij2[0][1] >= g.ny):
+                print('warning: periodic BC applied on y right')
+                ij2[0][1] = 0
+            c2 = g.sub2ind(ij2)
+            #print (c,s,pt,ij,ij2,c2)
+            jj.extend(c2)
+            aij.extend([diffWeights[s]])
+
+    print (len(aij), len(ii), len(jj))
+    print (len(g.band), g.nx*g.ny)
+    print (max(aij), max(ii), max(jj))
+    print (min(aij), min(ii), min(jj))
+
+    D = coo_matrix( (aij,(ii,jj)), shape=(len(g.band), g.nx*g.ny), dtype=type(g.dx) )
+    print "  D row " + str(len(g.band))
     print "elapsed time = " + str(time()-st)
     return D.tocsr()
+
+
+def buildDiffMatrixFast(g):
+    """
+    generate the matrix D
+    TODO: right now defaults to Laplacian but this should be a general
+    function
+    """
+    from math import log10,ceil
+    from scipy.sparse import coo_matrix
+    from time import time
+    from numpy import array as a
+
+    progout = 10 ** (ceil(log10(len(g.band)))-1)
+    print "building D"
+    st=time()
+
+    # get the stencil info
+    (f,stencil,armlen) = stencils.Laplacian_2nd(dim=2)
+    diffWeights = f(g.dx)
+    stencilsize = len(diffWeights)
+
+    # make empty lists for i,j and a_{ij}
+    ii = [];  jj = [];  aij = []
+
+    ij = g.ij
+    #I = g.band
+    #I2 = g.sub2ind(ij)
+
+    nx = g.nx
+    ny = g.ny
+    for s,pt in enumerate(stencil):
+        ij2 = ij + pt
+        # TODO, clean up, do this BC stuff in a separate function
+        # (ideally in a cleaner way, with some warning when this
+        # happens)
+        M1 = ij2 < a([0,-42])
+        M2 = ij2 < a([-42,0])
+        M3 = ij2 > a([nx-1,2*ny])
+        M4 = ij2 > a([2*nx,ny-1])
+        ij2 = (M1) * (nx-1) + (~M1) * ij2
+        ij2 = (M2) * (ny-1) + (~M2) * ij2
+        ij2 = (M3) * 0 + (~M3) * ij2
+        ij2 = (M4) * 0 + (~M4) * ij2
+
+        I2 = g.sub2ind(ij2)
+        #ii.extend(I)  # BUG!  this won't be banded in the rows
+        ii.extend(range(0,len(g.band)))
+        jj.extend(I2)
+        aij.extend([diffWeights[s]]*len(g.band))
+
+    print (len(aij), len(ii), len(jj))
+    print (len(g.band), g.nx*g.ny)
+    print (max(aij), max(ii), max(jj))
+    print (min(aij), min(ii), min(jj))
+    D = coo_matrix( (aij,(ii,jj)), shape=(len(g.band), g.nx*g.ny), dtype=type(g.dx) )
+    print "  D row " + str(len(g.band))
+    print "elapsed time = " + str(time()-st)
+    return D.tocsr()
+
+
+
+def buildExtensionMatrix(g, xy, degreep=3):
+    r"""
+    Generate the matrix E
+
+    Notes:
+    In the diff op case, the weights are fixed, here they depend on
+    which node we're at.
+    """
+    from math import ceil,log10
+    from scipy.sparse import coo_matrix
+    from time import time
+    from numpy import zeros
+
+    # TODO: dim hardcoded to 2 in some places in this function
+    dim = xy.shape[1]
+    dx = g.dx
+
+    relpt = a((g.x1d[0], g.y1d[0]))
+    #stencilsize = len(Levolve[0].interppts)
+    stencilsize = (degreep+1)**dim
+    progout = 10 ** (ceil(log10(len(g.band)))-1)
+    print "building E"
+    st=time()
+
+    # make empty lists the sparse matrix
+    # TODO: is it faster to use numpy arrays here?
+    ii = [];  jj = [];  aij = []
+
+    # how many points to interpolate
+    N = xy.shape[0]
+
+    # TODO: order here must match code elsewhere: this is a very bad idea
+    stencil = zeros( (stencilsize,dim) , dtype=int)
+    c = 0
+    for j in range(0,degreep+1):
+        for i in range(0,degreep+1):
+            stencil[c][0] = i
+            stencil[c][1] = j
+            c = c+1
+
+    # can do all of them at once
+    #bpij = findGridInterpBasePt(xy, dx, relpt, degreep)
+    for c in range(0, N):
+        if c % progout == 0:  print "  E row " + str(c)
+        x = xy[c]
+        bpij = findGridInterpBasePt(x, dx, relpt, degreep)
+        bpx = relpt + bpij * dx
+        interpweights = buildInterpWeights(bpx, x, dx, degreep+1)
+        interppts = g.sub2ind(bpij + stencil)
+
+        ii.extend([c]*stencilsize)
+        jj.extend(interppts)
+        aij.extend(interpweights)
+    #TODO: does this work with float96?
+    E = coo_matrix( (aij,(ii,jj)), shape=(len(g.band),g.nx*g.ny), dtype=type(dx) )
+
+    # TODO: return this info as well:
+    from numpy import unique
+    band2 = unique(jj)
+    print (len(jj),len(band2),len(g.band))
+    print "  E row " + str(len(g.band))
+    print "elapsed time = " + str(time() - st)
+    #return E.tocsr()
+    #E2 = E.tocsr()
+    #EE = E2[:,band2]
+    return (E.tocsr(), band2)
