3680b4a2a1c1aab6e2abc3377612128d0bebf4a2
Colin Macdonald <macdonald@maths.ox.ac.uk>
2012-02-16 23:30:17 +0000

    add a new triangulation and some code to read .OFF files

---
+++ surfaces/tri/offread.m
@@ -0,0 +1,48 @@
+function [Faces, Vertices] = offread(fname)
+%OFFREAD Read a triangulation from an ascii .off file
+%   [Faces, Vertices] = plyread('genus3.off');
+%
+%   Note: this may not be a standard off file.
+
+  f = fopen(fname);
+
+  s = fgetl(f);
+  assert(strcmpi(s, 'OFF'));
+
+  [temp] = fscanf(f, '%d %d', 2);
+  numv = temp(1);
+  numf = temp(2);
+  s = fgetl(f);  % get the rest of the numv numf 0 line
+
+  s = fgetl(f);  % a blank line?
+  assert(strcmp(s, ''));
+
+  Vertices = fscanf(f, '%g', [3, numv]);
+  Faces = fscanf(f, '%g', [4, numf]);
+
+  fclose(f);
+
+  %  3  ix iy iz
+  if any(Faces(1,:) ~= 3)
+    error('non-triangle in .off file')
+  end
+
+  Vertices = transpose(Vertices);
+
+  % discard triangles columns and c-based indexing to matlab
+  Faces = transpose(Faces(2:4,:)) + 1;
+
+  % debugging plot
+  if (1==0)
+    xp = Vertices(:,1);
+    yp = Vertices(:,2);
+    zp = Vertices(:,3);
+
+    up = sin(10*zp).*cos(7*xp).*sin(6*yp);
+
+    figure(1); clf;
+    trisurf(Faces,xp,yp,zp, up);
+    camlight left
+  end
+
+
