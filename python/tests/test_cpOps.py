ad8f0b266e5ac0e422c40f073c91fef4bdc44a50
Jorge Cañardo <jorgecanardo@gmail.com>
2012-07-19 14:45:01 +0100

    Fix test for the new LagrangeWeights1D behaviour.
    Maybe LagrangeWeights1D should test if np.isscalar(x) and if so behave like
    the previous version and return a 1D array instead of a 2D one?

--- python/tests/test_cpOps.py
+++ python/tests/test_cpOps.py
@@ -3,4 +3,4 @@ import numpy.testing
 
 def test_LagrangeWeights1D():
     from cp import cpOps
-    numpy.testing.assert_equal(cpOps.LagrangeWeights1D(-0.25, 0., 0.25, 4), np.array([0, 1, 0, 0]))
+    numpy.testing.assert_equal(cpOps.LagrangeWeights1D(-0.25, np.array([0.]), 0.25, 4), np.array([[0, 1, 0, 0]]))
