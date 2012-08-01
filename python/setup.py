12a1b82de5016d55a649792c75bfac2797fabe0c
Jorge Cañardo <jorgecanardo@gmail.com>
2012-08-01 12:16:38 +0100

    And setup.py changes

--- python/setup.py
+++ python/setup.py
@@ -3,5 +3,10 @@ from distutils.core import setup
 setup(
     name="cp",
     version="0.2dev",
-    packages=["cp",],
+    packages=["cp",
+              "cp.tools",
+              "cp.petsc",
+              "cp.surfaces",
+              "cp.tests",],
+    package_data={'cp.tests': ['data/*']},
 )
