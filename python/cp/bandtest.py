4a0ee000c68e262ae95ee8bb9d282ed0c1f6b9e3
nullas <sean.null@gmail.com>
2012-08-06 14:35:29 +0100

    find new way to handle indexing without create big vectors,need to rewrite some parts

--- python/cp/bandtest.py
+++ python/cp/bandtest.py
@@ -16,7 +16,7 @@ from mpi4py import MPI
 try:
     import petsc4py
     import sys
-#    from petsc4py import PETSc
+    from petsc4py import PETSc
     petsc4py.init(sys.argv)
     from numpy import array as a
     import numpy.testing as npt
@@ -27,9 +27,10 @@ class TestBand(unittest.TestCase):
 
 
     def setUp(self):
+        self.comm = MPI.COMM_WORLD
         surface = Surface()
-        self.bnd = Band(surface,MPI.COMM_WORLD)
-
+        self.bnd = Band(surface,self.comm)
+        self.bnd.createGLVectors()
 
     def tearDown(self):
         pass
@@ -55,27 +56,25 @@ class TestBand(unittest.TestCase):
             sub = self.bnd.BlockInd2SubWithoutBand(ind)
             rslt = self.bnd.BlockSub2CenterCarWithoutBand(sub)
             npt.assert_array_equal(rslt, right)
-    def testSurface(self):
-        '''test Surfaces.Sphere'''
-        surface = self.bnd.surface
-        example = ((a([0,0,0]),a([1,0,0])),
-                   (a([[-1,0,0],[0,-1,0]]),a([[-1,0,0],[0,-1,0]])),
-                   (a([[0,0,0]]),a([[1,0,0]])))
-        for x,y in example:
-            rlt,_,_,_ = surface.cp(x)
-            npt.assert_array_equal(y,rlt)
+
             
     def testVisually(self):
         '''visually blocks selected.'''
-        x = self.bnd.BlockSub2CenterCarWithoutBand(self.bnd.BlockInd2SubWithoutBand(self.bnd.gindBlockWBand.getArray()))
-#        self.bnd.gindBlockWBand.view()
+#        if self.comm.rank == 0:
+        g2z,zvec = PETSc.Scatter().toZero(self.bnd.gsubBlockWBand)
+        g2z.scatter(self.bnd.gsubBlockWBand,zvec, PETSc.InsertMode.INSERT)
+        x = self.bnd.BlockSub2CenterCarWithoutBand(zvec.getArray())
+        lx = self.bnd.BlockSub2CenterCarWithoutBand(self.bnd.gsubBlockWBand.getArray())
+        print x
         import pylab as pl
-        from mpl_toolkits.mplot3d import Axes3D
+        from mpl_toolkits.mplot3d import Axes3D #@UnusedImport
         fig = pl.figure()
         ax = fig.add_subplot(111, projection='3d')
-        ax.scatter3D(x[:,0],x[:,1],x[:,2])
-        pl.show()
+        ax.scatter3D(x[::self.bnd.Dim],x[1::self.bnd.Dim],x[2::self.bnd.Dim],c='blue',marker='o')
+        ax.scatter3D(lx[::self.bnd.Dim],lx[1::self.bnd.Dim],lx[2::self.bnd.Dim],c='red',marker='D')
+        pl.savefig('testVis.png')
         
+            
 
 
 if __name__ == "__main__":
--- python/cp/petsc/band.py
+++ python/cp/petsc/band.py
@@ -4,11 +4,21 @@ Created on Jul 29, 2012
 @author: nullas
 '''
 from __future__ import division
+from mpi4py import MPI
 from petsc4py import PETSc
 import scipy as sp
-from numpy import array as a
+from numpy import array 
 import exceptions
 
+
+#class harray(array):
+#    '''Providing hash for Array. Use this for indexing dictionary.'''
+#    def __init__(self,obj, dtype=None, copy=True, order=None, subok=False, ndmin=0):
+#        self.a = a.__init__(self,obj, dtype, copy, order, subok, ndmin)
+#        
+#    def __hash__(self):
+#        return hashlib.sha1(self.a)
+        
 
 class Band(object):
     '''
@@ -24,37 +34,142 @@ class Band(object):
         self.surface = surface
         OptDB = PETSc.Options()
         self.M = OptDB.getInt('M',20)
-        self.m = OptDB.getInt('m',10)
-        self.StencilWidth = OptDB.getInt('p',4)
+        self.m = OptDB.getInt('m',2)
+        self.StencilWidth = OptDB.getInt('p',1)
         self.Dim = OptDB.getInt('d',3)
         self.hBlock = 4/self.M
         self.hGrid = self.hBlock/self.m
         
-        numTotalBlocks = self.M**self.Dim
-        numBlocksAssigned = numTotalBlocks // comm.size + int(comm.rank < (numTotalBlocks % comm.size))
-        BlockStart = comm.exscan(numBlocksAssigned)
-        if comm.rank == 0:BlockStart = 0
-        indBlocks = sp.arange(BlockStart,BlockStart+numBlocksAssigned)
-        subBlocks = self.BlockInd2SubWithoutBand(indBlocks)
-        BlockCenterCar = self.BlockSub2CenterCarWithoutBand(subBlocks)
+    def SelectBlock(self,surface = None):
+        
+        if surface is None:
+            surface = self.surface
+        comm = self.comm
+        numTotalBlock = self.M**self.Dim
+        numBlockAssigned = numTotalBlock // comm.size + int(comm.rank < (numTotalBlock % comm.size))
+        Blocktart = comm.exscan(numBlockAssigned)
+        if comm.rank == 0:Blocktart = 0
+        indBlock = sp.arange(Blocktart,Blocktart+numBlockAssigned)
+        subBlock = self.BlockInd2SubWithoutBand(indBlock)
+        BlockCenterCar = self.BlockSub2CenterCarWithoutBand(subBlock)
         _,dBlockCenter,_,_ = surface.cp(BlockCenterCar)
         (lindBlockWithinBand,) = sp.where(dBlockCenter<1/2*\
                                           (self.hBlock-self.hGrid)*sp.sqrt(self.Dim))
-        lindBlockWithinBand = lindBlockWithinBand+BlockStart
-        lBlockSize = a(lindBlockWithinBand.size,'i')
-        numTotalBlocksWBand = comm.allreduce(lBlockSize)
-        self.numTotalBlocksWBand = numTotalBlocksWBand
-        self.numBlockWBandAssigned = numTotalBlocksWBand // comm.size + int(comm.rank < (numTotalBlocksWBand % comm.size))
-        gindBlocksWBand = PETSc.Vec().createMPI((lBlockSize,PETSc.DECIDE))
-        gindBlocksWBand.setArray(lindBlockWithinBand)
-        self.gindBlockWBand = PETSc.Vec().createMPI((self.numBlockWBandAssigned,PETSc.DECIDE))
-        ISAll = PETSc.IS().createStride(numTotalBlocksWBand,step=1,comm=comm)
-        scatter = PETSc.Scatter().create(gindBlocksWBand,ISAll,self.gindBlockWBand,None) 
-        scatter.scatter(gindBlocksWBand,self.gindBlockWBand,PETSc.InsertMode.INSERT)
-#        PETSc.Sys.syncPrint('Process {0} got {1} Blocks'.format(comm.rank,lBlockSize))
-#        PETSc.Sys.syncFlush()
-#        PETSc.Sys.Print('Total Blocks {0}'.format(self.numTotalBlocksWBand))
+        lindBlockWithinBand = lindBlockWithinBand+Blocktart
+        lBlockSize = lindBlockWithinBand.size
+        lsubBlockWBand = self.BlockInd2SubWithoutBand(lindBlockWithinBand)
+        numTotalBlockWBand = comm.allreduce(lBlockSize)
         
+        
+
+        numBlockWBandAssigned = numTotalBlockWBand // comm.size + int(comm.rank < (numTotalBlockWBand % comm.size))
+               
+        lsubBlockWBandFrom = PETSc.Vec().createWithArray(lsubBlockWBand,comm=comm)
+        self.gsubBlockWBand = PETSc.Vec().createMPI((self.Dim*numBlockWBandAssigned,PETSc.DECIDE),comm=comm)
+        
+        
+
+        
+
+        
+        
+#        gsubBlockWBandFrom = PETSc.Vec().createMPI((self.Dim*lBlockize,PETSc.DECIDE),comm=comm)
+#        gsubBlockWBandFrom.setArray(lsubBlockWBand)
+#        self.gsubBlockWBand = PETSc.Vec().createMPI((self.Dim*self.numBlockWBandAssigned,PETSc.DECIDE),comm=comm)
+        BlockWBandStart = comm.exscan(numBlockWBandAssigned)
+        if comm.rank == 0:
+            BlockWBandStart = 0
+        self.BlockWBandStart = BlockWBandStart
+        
+                
+        LInd = PETSc.IS().createStride(numBlockWBandAssigned*self.Dim,\
+                                       first=BlockWBandStart*self.Dim,\
+                                       step=1,comm=comm)
+
+
+        self.numTotalBlockWBand = numTotalBlockWBand
+        self.numBlockWBandAssigned = numBlockWBandAssigned
+        
+        BlockWBandEnd = BlockWBandStart + numBlockWBandAssigned
+        self.BlockWBandEnd = BlockWBandEnd
+        scatter = PETSc.Scatter().create(lsubBlockWBandFrom,LInd,self.gsubBlockWBand,None) 
+        scatter.scatter(lsubBlockWBandFrom,self.gsubBlockWBand,PETSc.InsertMode.INSERT)
+        
+        
+    def createGLVectors(self):
+        
+        self.SelectBlock()
+        
+
+        
+        self.larray = sp.zeros((self.numBlockWBandAssigned,)+\
+                               (self.m+self.StencilWidth*2,)*self.Dim,order='F')
+        self.lvec = PETSc.Vec().createWithArray(self.larray,comm=self.comm)
+        lsize = self.numBlockWBandAssigned*self.m**self.Dim
+        self.gvec = PETSc.Vec().createMPI((lsize,PETSc.DECIDE))
+        self.gvec.setUp()
+        
+#        self.createIndicesHelper()
+        tind = sp.arange((self.m+self.StencilWidth*2)**self.Dim)
+        tind = tind.reshape((self.m+self.StencilWidth*2,)*self.Dim,order='F')
+        
+        for dim in xrange(self.Dim):
+            tind = sp.delete(tind,0,dim)
+            tind = sp.delete(tind,sp.s_[-1],dim)
+            
+        tind = tind.flatten(order='F')
+        
+        ISList = []
+        c = (self.m+self.StencilWidth*2)**self.Dim
+        for i in xrange(self.BlockWBandStart,self.BlockWBandEnd):
+            ti = i*c
+            ISList.extend(list(tind+ti))
+            
+        ISFrom = PETSc.IS().createGeneral(ISList,comm=self.comm)
+        self.l2g = PETSc.Scatter().create(self.lvec,ISFrom,self.gvec,None)
+            
+        
+        
+        
+        
+        
+#        self.gsubBlockWBand.setArray(self.asubBlockWBan[self.Dim*BlockWBandStart:self.Dim*BlockWBandEnd])
+        
+        
+#        gindBlockWBandFrom = PETSc.Vec().createMPI((lBlockize,PETSc.DECIDE))
+#        gindBlockWBandFrom.setArray(lindBlockWithinBand)
+#        self.gindBlockWBand = PETSc.Vec().createMPI((self.numBlockWBandAssigned,PETSc.DECIDE))
+
+#        PETSc.Sys.syncPrint('Process {0} got {1} Block'.format(comm.rank,lBlockize))
+#        PETSc.Sys.syncFlush()
+#        PETSc.Sys.Print('Total Block {0}'.format(self.numTotalBlockWBand))
+
+
+#    def createIndicesHelper(self):
+#        toall,vsubBlockWBand = PETSc.Scatter().toAll(self.gsubBlockWBand)
+#        toall.scatter(self.gsubBlockWBand,vsubBlockWBand)
+#        asubBlockWBand = vsubBlockWBand.getArray().reshape(-1,self.Dim).astype(sp.int64)
+#        numTotalBlockWBand = self.numTotalBlockWBand
+#        self.sub2indWBand = {tuple(asubBlockWBand[i]):i for i in xrange(numTotalBlockWBand)}
+#        self.ind2subWBand = {i:asubBlockWBand[i] for i in xrange(numTotalBlockWBand)}
+#        x = sp.arange(3**self.Dim)
+#        x = sp.vstack(sp.unravel_index(x,(3,)*self.Dim,order='F')).T
+#        x -= sp.ones(self.Dim,dtype=sp.int64)
+#        lBlockSize = self.numBlockWBandAssigned
+#        for i in xrange(lBlockSize):
+#            sub = self.ind2subWBand[i]
+#            for offset in x:
+#                tsub = tuple(sub + offset)
+                
+    def createExtensionMat(self):     
+        '''create a real PETSc.Mat() for extension'''       
+        extMat = PETSc.Mat().create()
+        extMat.setO
+        extMat.setSizes((self.lvec.sizes,self.gvec.sizes))
+        
+        
+                        
+                
     
     def BlockInd2SubWithoutBand(self,index):
         if sp.isscalar(index):
@@ -64,20 +179,13 @@ class Band(object):
         if sp.isscalar(ind):
             if ind < 0 or ind > self.M**self.Dim-1:
                 raise exceptions.IndexError('BlockInd2SubWithoutBand')
-            sub = sp.zeros(self.Dim)
-            for i in range(0,self.Dim):
-                sub[i] = ind%self.M
-                ind //= self.M
-            return sub
+            return sp.column_stack(sp.unravel_index(ind,(self.M,)*self.Dim,order='F'))[0]
             
         else:
-            if ind.any() < 0 or ind.any() >self.M**self.Dim-1:
+            if ind.any() < 0 or ind.any() > self.M**self.Dim-1:
                 raise exceptions.IndexError('BlockInd2SubWithoutBand')
-            sub = sp.zeros((ind.size,self.Dim))
-            for i in range(0,self.Dim):
-                sub[:,i] = ind%self.M
-                ind //= self.M
-            return sub
+            return sp.column_stack(sp.unravel_index(ind,(self.M,)*self.Dim,order='F'))
+        
     def BlockSub2CenterCarWithoutBand(self,sub):       
         return sub/self.M*4-2+self.hBlock/2
         
