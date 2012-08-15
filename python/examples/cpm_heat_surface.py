01440e1a66b5bd6898937f7230c54d7b03eaf04a
nullas <sean.null@gmail.com>
2012-08-15 12:40:56 +0100

    to test on HPC

--- python/examples/cpm_heat_surface.py
+++ python/examples/cpm_heat_surface.py
@@ -22,28 +22,43 @@ def test_initialu(cp):
 def initialu(cp):
     return cp[:,0]
 
-def plot3d(x,gv):
-    v1 = PETSc.Vec().createWithArray(x)
-    v = band.toZeroStatic(v1)
-    v2 = band.toZeroStatic(gv)
-    pl.figure()
-    pl.points3d(v[0::3],v[1::3],v[2::3],v2,mode='point')
+def plot3d_sep(x,gv):
+    v2 = gv.getArray()
+    pl.figure(bgcolor=(1,1,1),fgcolor=(0.5,0.5,0.5))
+    pl.points3d(x[:,0],x[:,1],x[2::3],v2,mode='point')
+
+def plot3d_total(x,gv = None):
+    v = PETSc.Vec().createWithArray(x)
+    v = band.toZeroStatic(v)
+    v = v.getArray()
+    if gv is not None:
+        v2 = band.toZeroStatic(gv)
+        if comm.rank == 0:
+            pl.figure(bgcolor=(1,1,1),fgcolor=(0.5,0.5,0.5))
+            pl.points3d(v[0::3],v[1::3],v[2::3],v2.getArray(),mode='point')
+    else:
+        if comm.rank == 0:
+            pl.figure(bgcolor=(1,1,1),fgcolor=(0.5,0.5,0.5))
+            pl.points3d(v[0::3],v[1::3],v[2::3],mode='point')        
     
-def plot3dformesh(x,gv):
-    v2 = band.toZeroStatic(gv)
-    if MPI.COMM_WORLD.rank == 1:
-        pl.figure()
-        pl.points3d(x[:,0],x[:,1],v[:,2],v2,mode='point')
+def plot3dformesh(x,cv,f):
+    cv = band.toZeroStatic(cv)
+    if MPI.COMM_WORLD.rank == 0:
+        v2 = cv.getArray()
+        pl.figure(bgcolor=(1,1,1),fgcolor=(0.5,0.5,0.5))
+        pl.triangular_mesh(x[:,0],x[:,1],x[:,2],f,scalars=v2)
 
 if __name__ == '__main__':
-    opt = {'M':80,'m':5,'d':3}
-    surface = MeshWrapper('knot1.ply')
-    pl.triangular_mesh(surface.v[:,0],surface.v[:,1],surface.v[:,2],surface.f,opacity = 1)
+    opt = {'M':60,'m':5,'d':3}
+    surface = MeshWrapper('eight.ply')
+    pl.figure(bgcolor=(1,1,1),fgcolor=(0.5,0.5,0.5))
+    pl.triangular_mesh(surface.v[:,0],surface.v[:,1],surface.v[:,2],surface.f,scalars=surface.v[:,0],opacity = 1)
     comm = MPI.COMM_WORLD
     band = Band(surface,comm,opt)
     la,lv,gv,wv = band.createGLVectors()
-    v = band.getCoordinates() 
-    pl.points3d(v[:,0],v[:,1],v[:,2],mode='point')
+    v = band.getCoordinates()
+    centers = band.BlockInd2CenterCarWithoutBand(band.gindBlockWBand.getArray())
+    pl.points3d(centers[:,0],centers[:,1],centers[:,2],mode='point')
     dt = 0.3*band.dx**2
     vv = sp.array([[0,0,0],[1,0,0],[-1,0,0],[0,1,0],[0,-1,0],[0,0,1],[0,0,-1]])
     weights = sp.array([-6,1,1,1,1,1,1])*(dt/band.dx**2)
@@ -73,19 +88,43 @@ if __name__ == '__main__':
 #    ts.solve(gv)
     band.initialu(initialu)
     PETSc.Sys.Print('Initial')
-    plot3d(v,gv)
+    plot3d_total(v,gv)
     nextt = 0.1
-    PETSc.Sys.Print('Begin to solve')
+    PETSc.Sys.Print('Begin to solve.\n dt is {0}'.format(dt))
     for t in sp.arange(0,0,dt):
         L.multAdd(gv,gv,wv)
         M.mult(wv,gv)
         if t > nextt:
             nextt += 0.1
             PETSc.Sys.Print('time is {0}'.format(t))
+            
+    PETSc.Sys.Print('End to solve.')
+    
+    v = surface.v
 
-
-
-    plot3dformesh(v,gv)
+    vsize = v.shape[0]
+    PETSc.Sys.syncPrint('vsize is {0}'.format(vsize))
+    PETSc.Sys.syncFlush()
+    print PETSc.COMM_WORLD.size
+    print comm.rank
+    vAssigned = vsize // comm.size + int(comm.rank < (vsize % comm.size))
+    print 'why???'
+    PETSc.Sys.syncPrint('vAssigned is {0}'.format(vAssigned))
+    PETSc.Sys.syncFlush()
+    vstart = comm.exscan(vAssigned)
+    PETSc.Sys.syncPrint(vAssigned)
+    PETSc.Sys.syncFlush()
+    if comm.rank == 0:
+        vstart = 0
+    PETSc.Sys.syncPrint('[{0}] starts at {1}'.format(comm.rank,vstart))
+    PETSc.Sys.syncFlush()
+    mv = band.createExtensionMatForLoop(cp=v[vstart:vstart+vAssigned])
+    PETSc.Sys.syncPrint('build extMat')
+    PETSc.Sys.syncFlush()
+    cv = mv.getVecLeft()
+    mv.mult(gv,cv)
+    
+    plot3dformesh(v,cv,surface.f)
     pl.show()
     PETSc.Sys.Print('maximal is {0}'.format(gv.max()[1]))    
 del band,v   
