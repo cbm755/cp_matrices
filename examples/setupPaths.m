15ac99bb431556f2208a99bef51c7e1f755d79f2
Colin Macdonald <macdonald@maths.ox.ac.uk>
2012-08-20 12:24:47 +0100

    add an examples directory for matlab
    todo later: move all examples here

---
+++ examples/setupPaths.m
@@ -0,0 +1,9 @@
+%SETUPPATHS  Add paths for the Closest Point Method
+%
+%   TODO: need to make sure this works on Mac and Windows too.
+
+disp('Adding paths for cp_matrices');
+
+addpath('../surfaces');
+addpath('../cp_matrices');
+
