ee9d8fdf370e7a094a69e55fc5f3a2c4d33c5b40
Jorge Cañardo <jorgecanardo@gmail.com>
2012-08-20 14:57:57 +0100

    Mores hemisphere tests.
    They can still be improved quite a lot I think.

--- python/cp/tests/test_surfaces.py
+++ python/cp/tests/test_surfaces.py
@@ -6,7 +6,21 @@ from cp.surfaces.Hemisphere import Hemisphere
 
 class TestHemisphere:
     def test_cp_2d(self):
-        pass
+        h = Hemisphere(np.zeros(2))
+        points = np.array([[0, 0],
+                           [-1, -1],
+                           [-1, 1],
+                           [2, 2]])
+        cp, dist, bdy, _ = h.closestPointToCartesianVec(points)
+        for cp_i, dist_i, bdy_i, points_i in zip(cp, dist, bdy, points):
+            cp_i2, dist_i2, bdy_i2, _ = h.closestPointToCartesian(points_i)
+            self.check_cp_results((cp_i, dist_i, bdy_i),
+                                  (cp_i2, dist_i2, bdy_i2))
+
+    def check_cp_results(self, res_a, res_b):
+        for x, y in zip(res_a, res_b):
+            npt.assert_allclose(x, y)
+
     def test_cp_3d(self):
         points = np.array([[0, 0, 0],
                            [0, 0, -2],
@@ -16,6 +30,7 @@ class TestHemisphere:
         h = Hemisphere()
         cp, dist, bdy, _ = h.closestPointToCartesianVec(points)
         for cp_i, dist_i, bdy_i, points_i in zip(cp, dist, bdy, points):
+            # If only we were using py3... "*res_a, _ = "
             cp_i2, dist_i2, bdy_i2, _ = h.closestPointToCartesian(points_i)
-            for x, y in zip((cp_i, dist_i, bdy_i), (cp_i2, dist_i2, bdy_i2)):
-                npt.assert_allclose(x, y)
+            self.check_cp_results((cp_i, dist_i, bdy_i),
+                                  (cp_i2, dist_i2, bdy_i2))
