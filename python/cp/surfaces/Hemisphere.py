adbb49aef2de96be2c831465c364ea1ccf2196cc
Jorge Cañardo <jorgecanardo@gmail.com>
2012-09-05 12:15:03 +0100

    Clean attributes and methods' names.
    Remove unnecessary methods.

--- python/cp/surfaces/Hemisphere.py
+++ python/cp/surfaces/Hemisphere.py
@@ -14,53 +14,50 @@ from coordinate_transform import cart2pol, pol2cart, sph2cart
 
 class Hemisphere(ShapeWithBdy):
     def __init__(self, center=np.array([0.0, 0.0, 0.0]), radius=1.0):
-        self._center = center
-        self._radius = radius
-        self._dim = len(center)
+        self.center = center
+        self.radius = radius
+        self.dim = len(center)
         ll = center - radius
         rr = center + radius
         ll[-1] = center[-1]
-        self._bb = [ll, rr]
-        if self._dim == 2 or self._dim == 3:
+        self.bounding_box = [ll, rr]
+        if self.dim == 2 or self.dim == 3:
             self._hasParam = True
-        #self._dim = self._center.shape[0]
-        # TODO: superclass knows about dimension?
-        #super(Hemisphere, self).__init__()
 
-    def closestPointToCartesianVec(self, x):
+    def closest_point(self, x):
         x = np.atleast_2d(x)
-        r = np.sqrt(np.sum((x - self._center)**2, axis=1))
+        r = np.sqrt(np.sum((x - self.center)**2, axis=1))
         xm = x.copy()
         r_eq_0 = r == 0
-        xm[r_eq_0, 0] = self._center[0] + 1
+        xm[r_eq_0, 0] = self.center[0] + 1
         r[r_eq_0] = 1
-        dim = self._dim
+        dim = self.dim
         cpx = np.zeros(x.shape)
         bdy = np.zeros(x.shape[0])
-        below_semicircle = xm[:, -1] < self._center[-1]
+        below_semicircle = xm[:, -1] < self.center[-1]
         if dim == 2:
-            left = (xm[:, 0] <= self._center[0]) & below_semicircle
-            cpx[left] = self._center - np.array([self._radius, 0.])
+            left = (xm[:, 0] <= self.center[0]) & below_semicircle
+            cpx[left] = self.center - np.array([self.radius, 0.])
             bdy[left] = 1
             right = ~left & below_semicircle
-            cpx[right] = self._center + np.array([self._radius, 0.])
+            cpx[right] = self.center + np.array([self.radius, 0.])
             bdy[right] = 2
         elif dim == 3:
             # Here it doesn't matter if xm[below_semicircle, *] has
             # shape (0,) because adding a scalar "works" (ie, xx has
             # also shape (0,))
-            xx = xm[below_semicircle, 0] - self._center[0]
-            yy = xm[below_semicircle, 1] - self._center[1]
+            xx = xm[below_semicircle, 0] - self.center[0]
+            yy = xm[below_semicircle, 1] - self.center[1]
             th, _ = cart2pol(xx, yy)
-            xx, yy = pol2cart(th, self._radius)
+            xx, yy = pol2cart(th, self.radius)
             cpx[below_semicircle] = (np.column_stack((xx,
                                                       yy ,
                                                       np.zeros(xx.shape))) +
-                                     self._center)
+                                     self.center)
             bdy[below_semicircle] = 1
         else:
             raise NotImplementedError(
-                'Dim {0} and higher not implemented'.format(dim)
+                'Dim {0} not implemented'.format(dim)
                 )
         rest = ~below_semicircle
         
@@ -68,74 +65,60 @@ class Hemisphere(ShapeWithBdy):
         # to add an array results in a ValueError: operands could not
         # be broadcast together
         if rest.any():
-            c = (self._radius / r[rest])[:, np.newaxis]
-            cpx[rest] = c * (xm[rest] - self._center) + self._center
+            c = (self.radius / r[rest])[:, np.newaxis]
+            cpx[rest] = c * (xm[rest] - self.center) + self.center
             bdy[rest] = 0
         dist = np.sqrt(np.sum((x - cpx)**2, axis=1))
         return cpx, dist, bdy, {}
         
-    def closestPointToCartesian(self, x):
-        r = np.linalg.norm(x - self._center, 2)
+    def closest_point_loop(self, x):
+        r = np.linalg.norm(x - self.center, 2)
         if r==0:
-            tx = self._center[0] + 1.0
+            tx = self.center[0] + 1.0
             r = 1.0
         else:
             tx = x[0]
         xm = x.copy()
         xm[0] = tx
 
-        dim = self._dim
+        dim = self.dim
 
-        if xm[-1] < self._center[-1]:
+        if xm[-1] < self.center[-1]:
             # "below" semicircle,
             if dim == 2:
-                if xm[0] <= self._center[0]:  # left
-                    cpx = self._center - np.array([self._radius, 0.0])
+                if xm[0] <= self.center[0]:  # left
+                    cpx = self.center - np.array([self.radius, 0.0])
                     bdy = 1
                 else:  # right
-                    cpx = self._center + np.array([self._radius, 0.0])
+                    cpx = self.center + np.array([self.radius, 0.0])
                     bdy = 2
             elif dim == 3:
-                xx = xm[0] - self._center[0]
-                yy = xm[1] - self._center[1]
+                xx = xm[0] - self.center[0]
+                yy = xm[1] - self.center[1]
                 th, r = cart2pol(xx,yy)
-                xx, yy = pol2cart(th, self._radius)
-                cpx = self._center + np.array([xx,yy,0])
+                xx, yy = pol2cart(th, self.radius)
+                cpx = self.center + np.array([xx,yy,0])
                 bdy = 1
             else:
                 # general case possible?
                 raise NotImplementedError(
-                    'Dim {0} and higher not implemented'.format(dim)
+                    'Dim {0} not implemented'.format(dim)
                     )
         else:
             # at or "above" semicircle: same as for whole circle
-            c = self._radius / r
-            cpx = c*(xm-self._center) + self._center
+            c = self.radius / r
+            cpx = c*(xm-self.center) + self.center
             bdy = 0
 
         dist = np.linalg.norm(cpx - x, 2)
         return cpx, dist, bdy, {}
 
-    def closestPointToCartesianGrid(self, grid):
-        """ Hack to accept grid arrays, since np.vectorize doesn't
-        work for class methods (see
-        mail.scipy.org./pipermail/numpy-discussion/2007-February/026060.html
-
-        FIXME It looses the actual closestpoint, and bdy"""
-        ret = np.empty(grid[0].shape).ravel()
-        gr = grid.reshape((3, -1))
-        for i in xrange(len(ret)):
-            ret[i] = self.closestPointToCartesian(gr[:, i])[1]
-        return None, ret.reshape(grid[0].shape), None, None
-        
-    cp = closestPointToCartesian
-
-    def ParamGrid(self, rez=10):
+    def parametric_grid(self, rez=10):
         """
         Return a mesh, for example for plotting with mlab.
         """
-        rad = self._radius
-        cen = self._center
+        rad = self.radius
+        cen = self.center
         # parametric variables
         #u=np.r_[0:2*pi:10j]
         #v=np.r_[0:0.5*pi:10j]
@@ -146,7 +129,6 @@ class Hemisphere(ShapeWithBdy):
         # TODO: return a list?  In case we have multiple components
         return x + cen[0], y + cen[1], z + cen[2]
 
-
 class Semicircle(Hemisphere):
     """
     Closest point represenation of a semicircle.
@@ -154,12 +136,12 @@ class Semicircle(Hemisphere):
     def __init__(self, center=np.array([0.0, 0.0]), radius=1.0):
         Hemisphere.__init__(self, center=center, radius=radius)
 
-    def ParamGrid(self, rez=256):
+    def parametric_grid(self, rez=256):
         """
         Parameterized form (for plotting).
         """
         th = np.linspace(0, np.pi, rez)
-        circ = self._radius * np.exp(1j*th)
-        X = np.real(circ) + self._center[0]
-        Y = np.imag(circ) + self._center[1]
+        circ = self.radius * np.exp(1j*th)
+        X = np.real(circ) + self.center[0]
+        Y = np.imag(circ) + self.center[1]
         return X, Y
