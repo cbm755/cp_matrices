c9e4f7eb8c9f50801f6ab7ff48154e0b3cb8cfe2
Jorge Cañardo <jorgecanardo@gmail.com>
2012-09-05 12:38:56 +0100

    Move things that are no longer used or haven't been cleaned up to TODO directories where they used to be.

--- python/cp/surfaces/__init__.py
+++ python/cp/surfaces/__init__.py
@@ -5,13 +5,7 @@ TODO: can we load all the various classes automatically here?
 from Hemisphere import Hemisphere
 from Sphere import Sphere
 from Circle import Circle
-from Multi import Multi
 from CPBar import CPBar
-from Manipulate import Translate  # Doesn't seem very useful
-from ParamCurve import ParamCurve
-from SplineCurve import SplineCurve
-from CircularArc import CircularArc
-from Cylinder import Cylinder
 from triangulation_fast import FindClosestPointToTriSet
 
 # TODO:
