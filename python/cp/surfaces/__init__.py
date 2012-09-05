37224215fff470e8f66ac32f56784f8f98b3b4a7
Jorge Cañardo <jorgecanardo@gmail.com>
2012-09-05 18:41:16 +0100

    I forgot to add Mesh to __init__

--- python/cp/surfaces/__init__.py
+++ python/cp/surfaces/__init__.py
@@ -6,6 +6,7 @@ from Hemisphere import Hemisphere
 from Sphere import Sphere
 from Circle import Circle
 from CPBar import CPBar
+from Mesh import Mesh
 from triangulation_fast import FindClosestPointToTriSet
 
 # TODO:
