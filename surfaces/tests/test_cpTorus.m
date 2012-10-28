622c33802548f4b6fe1ea6905d8c695fee6f271e
Colin Macdonald <macdonald@maths.ox.ac.uk>
2012-10-28 09:59:42 +0000

    one more test for the torus code

--- surfaces/tests/test_cpTorus.m
+++ surfaces/tests/test_cpTorus.m
@@ -24,10 +24,19 @@ function [pass, str] = test_cpTorus()
   c = c + 1;
   pass(c) = assertAlmostEqual([cpx,cpy,cpz,sdist], [1,0,0.4,0]);
 
-  % from origin, check cpz is zero and distance is right
+  % from origin, check cpz is zero and distance is correct
   [cpx,cpy,cpz,sdist] = cpTorus(0,0,0);
   c = c + 1;
   pass(c) = assertAlmostEqual([sqrt(cpx^2+cpy^2),cpz,sdist], [0.6,0,0.6]);
+
+  % from inner medial axis, check distance
+  th = linspace(0,2*pi,50);
+  [x,y] = pol2cart(th, 1.5);
+  z = zeros(size(x));
+  [cpx,cpy,cpz,sdist] = cpTorus(x,y,z, 1.5, 0.5);
+  c = c + 1;
+  pass(c) = assertAlmostEqual([cpz,sdist], ...
+                              [zeros(size(cpx)),-0.5*ones(size(sdist))]);
 
   % check the parameters
   [cpx,cpy,cpz,sdist] = cpTorus(2,0,0,10,2);
