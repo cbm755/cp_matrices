9b153945761007f1683136081e7d72b9742a8ed9
Colin B. Macdonald <cbm@m.fsf.org>
2017-06-19 12:04:44 -0700

    porcupine plot is faster, code a bit better

--- cp_matrices/porcupine_plot3d_param.m
+++ cp_matrices/porcupine_plot3d_param.m
@@ -6,34 +6,42 @@ function porcupine_plot3d_param(x, y, z, cpx, cpy, cpz, bdy, paramf, fignum)
 %      paramf is the name of function that return a parameterization
 %      fignum must already exist.  Works for codim-1 (surfaces) and
 %      codim-2 (curves/filaments).
+%
+%   pass empty to paramf to skip drawing the surface.
 
-  [xp,yp,zp] = paramf(64);
+  onlyPlotSome = false;
+
+  if (isempty(bdy))
+    bdy = zeros(size(cpx));
+  end
   figure(fignum); clf;
-  if isvector(xp)
-    plot3(xp,yp,zp, 'g-', 'linewidth',4);
-  else
-    surf(xp,yp,zp,zeros(size(xp)));
-    shading flat
+  if (~ isempty(paramf))
+    [xp,yp,zp] = paramf(64);
+    if isvector(xp)
+      plot3(xp,yp,zp, 'g-', 'linewidth',4);
+    else
+      surf(xp,yp,zp,zeros(size(xp)));
+      shading flat
+    end
   end
   hold on;
   xlabel('x'); ylabel('y'); zlabel('z');
-  N = length(x(:));
-  %N = 1000;
-  bdycolorset = {'r' 'b' 'm' 'c' 'y'};
-  for j=1:N
-    %i = ceil(rand*NN);
-    i = j;
-    %if (abs(z(i)) < 0.2)
-      if (bdy(i) == 0)
-        plot3([x(i) cpx(i)], [y(i) cpy(i)], [z(i) cpz(i)], 'k-');
-        plot3(cpx(i),cpy(i),cpz(i), 'k.');
-      else
-        h1 = plot3([x(i) cpx(i)], [y(i) cpy(i)], [z(i) cpz(i)], '-');
-        h2 = plot3(cpx(i),cpy(i),cpz(i), '-');
-        set(h1, 'color', bdycolorset{bdy(i)});
-        set(h2, 'color', bdycolorset{bdy(i)});
-      end
-      %end
+  axis equal
+  bdycolors = {'k' 'r' 'b' 'm' 'c' 'y'};
+  uniqbdy = unique(bdy);
+  for c = 1:length(uniqbdy)
+    I = (bdy == uniqbdy(c));
+    if (onlyPlotSome)
+      I = find(I);
+      n = numel(I);
+      % random
+      r = randperm(n); r = r(1:min(500,n));
+      % or take the first 500
+      %r = 1:min(500, n);
+      I = I(r);
+    end
+    plot3(cpx(I), cpy(I), cpz(I), [bdycolors{c} '.']);
+    plot3([x(I) cpx(I)]', [y(I) cpy(I)]', [z(I) cpz(I)]', [bdycolors{c} '-']);
   end
 
   axis equal
