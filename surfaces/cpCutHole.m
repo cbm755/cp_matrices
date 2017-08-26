e85e118455e8e61e53042b19c32ffec6f6cbd779
Colin B. Macdonald <cbm@m.fsf.org>
2017-08-25 17:25:22 -0700

    start making cpCutHole take cpf argument

--- surfaces/cpCutHole.m
+++ surfaces/cpCutHole.m
@@ -1,46 +1,52 @@
-function [cpx,cpy,cpz,dist,bdy,SA_final] = cpVase_w_holesSA(x, y, z, ...
-    lim,ab,cen,holes,tol,SA_wanted,dx,bw)
-%cpf, holes,...
-
+function [cpx,cpy,cpz,dist,bdy,SA_final] = cpCutHole(x,y,z,cpf,hole_cen,SA_target,tol,dx,bw,cpx0,cpy0,cpz0)
+%cpCutHole  Cut holes in surfaces described by cpfun
+%
 % x,y,z coordinate matrices of any same dimension
-% lim = [angle from xy-plane, angle from xy-plane], a range of angle.
-%   e.g. lim = [-pi/2, pi/2] makes a full ellipsoid
+%
+% hole_cen = [hx hy hz] gives the center of the hole.  If this is not on
+% the surface given by cpf, it will be projected onto the surface (by
+% calling cpf.
+%
+% Multiple holes are supported: pass each center as a row of "hole_cen"
+% and pass a vector to "SA_target"
 
+  warning('WIP')
+  % TODO: if nargout is 5, get bdy from cpf instead
+  %nargout(cpf)
+  %tic
+  % TODO: at least for triangulated surface, need a faster approach
+  % TODO: we could optionally pass cpx0 etc as previously computed cp data.
+  [cpx, cpy, cpz, tilde] = cpf(x, y, z);
+  %toc
+  %[cpx, cpy, cpz, tilde, tilde] = cpf(x, y, z);
+  bdy = zeros(size(cpx));
+  [num_holes, temp] = size(hole_cen);
+  assert (temp == 3)
 
-% just project onto vase
-%cpf = @(x,y,z) cpVase(x, y, z, lim, ab, cen);
-%[cpx,cpy,cpz,~,~] = cpf;
+  for k=1:num_holes
+    hole = hole_cen(k,:);
 
-[cpx,cpy,cpz,~,~] = cpVase(x, y, z, lim, ab, cen);
-bdy = zeros(size(cpx));
-size_holes=size(holes);
-num_holes=size_holes(1);
-
-for k=1:num_holes
-    hole = holes(k,:);
-    
     xh = hole(1);
     yh = hole(2);
     zh = hole(3);
-    Rh = hole(4);
-    
-    if exist('SA_wanted','var')
+
+
         
-        SA_wanted_k = SA_wanted(k);
+        SA_wanted_k = SA_target(k);
         R_bar = sqrt(SA_wanted_k/pi);
         RR = linspace(0.8*R_bar,1.2*R_bar, 50);
         for i = 1:length(RR)          
             I = ((cpx-xh).^2+(cpy-yh).^2+(cpz-zh).^2 < RR(i)^2);
             SASA(i) = sum(I(:))*(dx)^2/bw;
         end
-          [~,II] = min(abs(SASA./SA_wanted_k-1));
+          [tilde,II] = min(abs(SASA./SA_wanted_k-1));
           R_H = RR(II);
           SA_final = SASA(II);
               
-    else
-        I = ((cpx-xh).^2+(cpy-yh).^2+(cpz-zh).^2 < Rh^2);
-        SA_final = 0;
-    end
+    %else
+    %    I = ((cpx-xh).^2+(cpy-yh).^2+(cpz-zh).^2 < Rh^2);
+    %    SA_final = 0;
+    %end
     
     
     
@@ -57,11 +63,11 @@ for k=1:num_holes
     while  abs(sdist) > tol | abs(dist1) > tol
        
         % project onto sphere "hole"
-        [xl2,yl2,zl2,sdist] = cpSphere(xl,yl,zl,Rh,[xh,yh,zh]);
+        [xl2,yl2,zl2,sdist] = cpSphere(xl,yl,zl,R_H,[xh,yh,zh]);
                         
         % project onto surface
-        %[xll,yll,zll,dist1] = cpFromTriSlow(xl2, yl2, zl2, Faces, Vertices);
-        [xll,yll,zll,dist1,~] = cpVase(xl2, yl2, zl2, lim, ab, cen);
+        [xll,yll,zll,dist1] = cpf(xl2, yl2, zl2);
+        %[xll,yll,zll,dist1,tilde] = cpVase(xl2, yl2, zl2, lim, ab, cen);
          xl=xll;
          yl=yll;
          zl=zll;
@@ -69,7 +75,7 @@ for k=1:num_holes
         % avoid projecting back and forth infinite times
         dist2=sqrt((xl-xll).^2 + (yl-yll).^2 + (zl-zll).^2);
         dist3=sqrt((xl-xl2).^2 + (yl-yl2).^2 + (zl-zl2).^2);
-        if  dist2  < tol2 & dist3 > Rh/2
+        if  dist2  < tol2 & dist3 > R_H/2
            xll = xll+10^(-5)*rand; % add perturbation
            yll = yll+10^(-5)*rand;
            zll = zll+10^(-5)*rand;
