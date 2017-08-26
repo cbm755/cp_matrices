b5b300274c1f9fa67cb38c915f7e69c530ad9b2a
Colin B. Macdonald <cbm@m.fsf.org>
2017-08-26 09:11:19 -0700

    cutting holes: 2d fcn and example

---
+++ examples/example_ellipse_hole.m
@@ -0,0 +1,85 @@
+%% Parameters
+tol = 10^-2;
+
+a = 1.6; % length along x
+b = 0.5; % length along y
+cpf = @(x, y) cpEllipse(x, y, a, b);
+paramf = @(N) paramEllipse(N, a, b);
+
+max_vec=[];
+sum_vec=[];
+dx=0.1; % grid size
+
+
+x1d = (-2.0:dx:2.0)';
+y1d = x1d;
+
+%% Banding: do calculation in a narrow band around the vase
+dim = 2;    % dimension
+p = 3;      % interpolation degree
+order = 2;  % Laplacian order
+% "band" is a vector of the indices of the points in the computation
+% band.  The formula for bw is found in [Ruuth & Merriman 2008] and
+% the 1.0001 is a safety factor.
+bw = 1.0001*sqrt((dim-1)*((p+1)/2)^2 + ((order/2+(p+1)/2)^2));
+
+% meshgrid is only needed for finding the closest points, not afterwards
+[xx, yy] = meshgrid(x1d, y1d);
+
+[cpx, cpy, dist] = cpf(xx, yy);
+
+% TODO: currently need to band before cutting b/c SA calc depends on bw
+band = find(abs(dist) <= bw*dx);
+
+% keep only closest points in the band
+cpx = cpx(band); cpy = cpy(band);
+x = xx(band); y = yy(band);
+
+theta=[pi/2];
+%theta = linspace(-pi/2,pi/2,1);
+
+for j= 1:length(theta)
+  xh=a*cos(theta(j));
+  yh=b*sin(theta(j));
+
+  len_wanted=2;
+  hole_cen = [xh yh];
+
+  [cpx,cpy,dist,bdy] = cpCutHole2d(x, y, cpf, hole_cen, len_wanted, tol, dx, bw);
+
+  %% Extension of closest point
+  E = interp2_matrix(x1d, y1d, cpx, cpy, p, band);
+
+  %% bdy cond;
+  E(bdy,:) = -E(bdy,:);
+
+  L = laplacian_2d_matrix(x1d,y1d, order, band, band);
+
+
+  %% For constant D
+  d = 1;
+  D = -1/d*ones(length(E),1);
+
+  %% solution of u
+  % boundary condition
+
+  gamma = 2*dim/(dx^2);
+  I = speye(size(E));
+  M = E*L - gamma*(I-E);
+  u = M \ D;
+  %u = gmres(M,D);
+
+  %% plot
+  N = 512;
+  [xp, yp, thp] = paramf(N);
+  Eplot = interp2_matrix(x1d, y1d, xp(:), yp(:), p, band);
+
+  up = Eplot*u;
+
+  figure(1); clf;
+  plot(thp, up);
+  xlabel('theta');
+
+  figure(2); clf;
+  plot2d_compdomain(u,x,y,dx,dx, 2)
+end
---
+++ surfaces/cpCutHole2d.m
@@ -0,0 +1,105 @@
+function [cpx,cpy,dist,bdy] = cpCutHole2d(x,y,cpf,hole_cen,SA_target,tol,dx,bw,given_cp_data)
+%cpCutHole2d  Cut holes in surfaces described by cpfun
+%
+% x,y coordinate matrices of any same dimension
+%
+% hole_cen = [hx hy] gives the center of the hole.  If this is not on
+% the surface given by cpf, it will be projected onto the surface (by
+% calling cpf).
+%
+% Multiple holes are supported: pass each center as a row of "hole_cen"
+% and pass a vector to "SA_target"
+
+  warning('WIP')
+  if (nargin >= 10)
+    cpx = given_cp_data{1};
+    cpy = given_cp_data{2};
+  else
+    [cpx, cpy] = cpf(x, y);
+    %[cpx, cpy, cpz, tilde, tilde] = cpf(x, y, z);
+  end
+  bdy = zeros(size(cpx));
+  [num_holes, temp] = size(hole_cen);
+  assert (temp == 2)
+
+  %code below assumes cpx, cpy are long vectors
+  savesz = size(cpx);
+  cpx = cpx(:);
+  cpy = cpy(:);
+  bdy = bdy(:);
+
+  for k=1:num_holes
+    xh = hole_cen(k, 1)
+    yh = hole_cen(k, 2)
+    % project onto surface
+    [xh yh] = cpf(xh, yh)
+
+    % look for a sphere radius which gives approx the target arclength
+    R_bar = SA_target(k)/2
+    RR = linspace(0.7*R_bar,1.02*R_bar, 100);
+    for i = 1:length(RR)
+      I = ((cpx-xh).^2 + (cpy-yh).^2 < RR(i)^2);
+      assert(nnz(I) > 0, 'empty circle/surface intersection!')
+      SASA(i) = sum(I(:))*(dx)/(2*bw);
+    end
+    SASA./SA_target(k)
+    [tilde,i] = min(abs(SASA./SA_target(k)-1))
+    R_H = RR(i)
+    assert (i > 1 && i < length(RR), 'should be in the middle')
+    assert (tilde < 0.1)
+
+    % index of all points whose cp are within the sphere
+    I = ((cpx-xh).^2 + (cpy-yh).^2 < R_H^2);
+
+    %% TODO: temp plotting
+    figure(10);
+    porcupine_plot2d(x, y, cpx, cpy);
+    plot(xh, yh, 'r*', 'linewidth', 5);
+    plot(cpx(I), cpy(I), 'r.');
+    plot(x(I), y(I), 'bo');
+    t = exp(1i*linspace(0,2*pi,64));
+    plot(xh + R_bar*real(t), yh + R_bar*imag(t), 'c:');
+    plot(xh + R_H*real(t), yh + R_H*imag(t), 'r-');
+
+
+    %% fixed-point iteration
+    xl = cpx(I);
+    yl = cpy(I);
+
+    sdist = 1;
+
+    tol2 = 0.01;
+
+    while (abs(sdist) > tol | abs(dist1) > tol)
+      % project onto sphere "hole"
+      [xl2, yl2, sdist] = cpCircle(xl, yl, R_H, [xh yh]);
+
+      % project onto surface
+      [xll, yll, dist1] = cpf(xl2, yl2);
+      xl=xll;
+      yl=yll;
+
+      % avoid projecting back and forth infinite times
+      dist2=sqrt((xl-xll).^2 + (yl-yll).^2);
+      dist3=sqrt((xl-xl2).^2 + (yl-yl2).^2);
+      if  dist2  < tol2 & dist3 > R_H/2
+        xll = xll+10^(-5)*rand; % add perturbation
+        yll = yll+10^(-5)*rand;
+      end
+
+      xl=xll;
+      yl=yll;
+    end
+
+    cpx(I) = xl;
+    cpy(I) = yl;
+
+    bdy = bdy | I;
+end
+
+
+  cpx = reshape(cpx, savesz);
+  cpy = reshape(cpy, savesz);
+  bdy = reshape(bdy, savesz);
+  dist = sqrt((cpx-x).^2 + (cpy-y).^2);
+end
