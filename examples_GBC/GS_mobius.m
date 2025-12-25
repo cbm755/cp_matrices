1675d2ba4032bf950e8787b5e30513e8d8de97a1
Tony Wong <kawahtony@gmail.com>
2025-12-25 13:34:32 -0800

    Add README and code for GBC paper

---
+++ examples_GBC/GS_mobius.m
@@ -0,0 +1,207 @@
+%% GS on mobius strip
+
+clear ; close all ; % clc ; 
+
+rng(1989)
+
+
+%% Construct a grid in the embedding space
+
+% grid size
+dx = 0.05 ;
+
+% make vectors of x, y, positions of the grid
+x1d = (-1.8:dx:1.8)' ;
+y1d = x1d ;
+z1d = x1d ;
+
+[xx, yy, zz] = meshgrid(x1d, y1d, z1d) ;
+
+% Find closest points on the surface
+R = 1 ; % radius of center circle
+T = 0.35 ; % thickness
+cpf = @cpMobiusStrip ; 
+[cpbarx, cpbary, cpbarz, dist, bdy] = cpbar_3d(xx, yy, zz, cpf, R, T) ;
+[cpx, cpy, cpz, ~, ~] = cpf(xx, yy, zz) ;
+
+% dx = 0.025 ;
+% 
+% load(['mobius_dx=', num2str(dx), '.mat']) ;
+
+
+%% Banding: do calculation in a narrow band around the surface
+
+dim = 3 ;    % dimension
+p = 1 ;      % Low interp degree
+q = 3 ;      % High interp degree
+order = 2 ;  % Laplacian order
+bw = 1.0001*sqrt((dim-1)*((q+1)/2)^2 + ((order/2+(q+1)/2)^2));
+band = find(abs(dist) < bw*dx);
+
+
+xx_band = xx(band) ; 
+yy_band = yy(band) ; 
+zz_band = zz(band) ; 
+cpbarx_band = cpbarx(band) ; 
+cpbary_band = cpbary(band) ; 
+cpbarz_band = cpbarz(band) ;
+dist_band = dist(band) ;
+bdy_band = bdy(band) ;
+cpx_band = cpx(band) ; 
+cpy_band = cpy(band) ; 
+cpz_band = cpz(band) ;
+
+
+%% Construct an interpolation matrix for closest point
+
+% Interpolation matrice for modified closest point (Low and high order)
+Ebarqmat = interp3_matrix(x1d,y1d,z1d, cpbarx_band, cpbary_band, cpbarz_band, q, band);
+
+% Interpolation matrice for closest point 
+Eqmat = interp3_matrix(x1d,y1d,z1d, cpx_band, cpy_band, cpz_band, q, band);
+
+
+%% Create Laplacian matrix 
+
+Lmat = laplacian_3d_matrix(x1d, y1d, z1d, order, band) ;
+
+
+%% Construct an interpolation matrix for plotting on sphere
+
+% % plotting grid on sphere, based on parametrization
+[xp,yp,zp] = paramMobiusStrip(200) ;
+xp1 = xp(:) ; yp1 = yp(:) ; zp1 = zp(:) ;
+
+% % Eplot is a matrix which interpolates data onto the plotting grid
+Eplot = interp3_matrix(x1d, y1d, z1d, xp1, yp1, zp1, 3, band) ;
+
+
+%% Parameters and nonlinearities for Gray--Scott
+
+% F = 0.045 ;  k = 0.06 ;  Du = 8e-5 ;  Dv = 4e-5 ; % spot (NBC)
+% F = 0.052 ;  k = 0.063 ;  Du = 8e-5 ;  Dv = 4e-5 ; % stripe (NBC)
+% F = 0.037 ;  k = 0.06 ; Du = 8e-5 ; Dv = 4e-5 ; % Labyrinthine (NBC)
+% F = 0.020 ;  k = 0.052 ;  Du = 8e-5 ;  Dv = 4e-5 ; % chaos
+F = 0.010 ;  k = 0.042 ;  Du = 8e-5 ;  Dv = Du/2.5 ; % spiral wave with kappa = 70
+
+% Robin coefficient
+kappa = 10 ; 
+
+% Reaction terms
+f = @(u,v) (-u.*v.*v + F*(1-u)) ;
+g = @(u,v) ( u.*v.*v - (F+k)*v) ;
+
+
+%% Initial conditions - small perturbation from steady state
+
+um = real(1 - sqrt(1 - 4*(F+k)^2/F))/2 ;
+vm = (F + k)/um ;
+uvec = um + 0.1*(rand(size(xx_band)) - 0.5) ;
+vvec = vm + 0.1*(rand(size(xx_band)) - 0.5) ;
+
+
+%% Matrices for time-stepping
+
+dvec = zeros(size(band)) ; 
+for k = 1:length(band)
+    if bdy_band(k) == 1  
+        % Compute unit outward conormal
+        % u = atan2(cpy_band(k), cpx_band(k) + R*T/2) ;
+        % n = [R*T*cos(u/2)*cos(u) ; 
+        %      R*T*cos(u/2)*sin(u) ; 
+        %      R*T*sin(u/2)] ;
+        % n = n/norm(n) ;
+
+        % % Compute unit outward conormal
+        n = [cpx_band(k); cpy_band(k); cpz_band(k)] - [cpbarx_band(k); cpbary_band(k); cpbarz_band(k)]  ;
+        n = n/norm(n) ;
+
+        % Compute vector pointing from surface to boundary grid
+        w = [xx_band(k); yy_band(k); zz_band(k)] - [cpx_band(k); cpy_band(k); cpz_band(k)] ;
+        % Make boundary normal outward
+        if dot(n, w) < 0 
+            n = -n ; 
+        end 
+
+        if norm([cpx_band(k)-cpbarx_band(k); cpy_band(k)-cpbary_band(k); cpz_band(k)-cpbarz_band(k)],2) >= 1e-4
+            dvec(k) = 2*(w'*n) ;
+        end
+    end
+end
+
+
+numpts = length(band) ;
+Dmat = spdiags(dvec, 0, numpts, numpts) ;
+
+gamma = 2*dim*max(Du,Dv)/(dx^2);
+Imat = speye(size(Ebarqmat)) ;
+Au = Du*(Ebarqmat*Lmat) - gamma*(Imat - Ebarqmat - Dmat*(-kappa*Eqmat)) ;
+Av = Dv*(Ebarqmat*Lmat) - gamma*(Imat - Ebarqmat - Dmat*(-kappa*Eqmat)) ;
+
+
+%% Record video
+
+videoname = ['GS_mobius_kappa=', num2str(kappa), '_dx=', num2str(dx)] ;
+writerObj = VideoWriter(videoname, 'MPEG-4') ;
+writerObj.FrameRate = 30 ; 
+
+% Open the video writer
+open(writerObj);
+
+
+%% Time-stepping
+
+Tf = 10000 ; % Simulation time
+dt = .1 * (1/max(Du,Dv))*dx^2 ; % Time step
+numtimesteps = ceil(Tf/dt) ; % Number of time steps
+dt = Tf/numtimesteps ; % Adjust for integer number of steps
+
+
+figure(1) ; clf ;
+sphplot = Eplot*uvec;
+sphplot = reshape(sphplot, size(xp));
+Hplot = surf(xp, yp, zp, sphplot);
+axis equal ; axis off ;
+view(-38.9, 33.6)
+shading interp
+colorbar
+colormap('jet')
+clim([0,1])
+% writeVideo(writerObj, getframe(gcf)) ; 
+
+
+t = 0 ;
+kt  = 0 ;
+
+while t < Tf
+
+  uvec0 = uvec ;
+  vvec0 = vvec ;
+
+  uvec = uvec0 + dt*( Ebarqmat*f(uvec0,vvec0) + Au*uvec0 ) ;
+  vvec = vvec0 + dt*( Ebarqmat*g(uvec0,vvec0) + Av*vvec0 ) ;
+  
+
+  if ( (mod(kt,25)==0) || (kt<=10) || (kt==numtimesteps) )
+    disp([kt t]);
+    sphplot = Eplot*uvec;
+    sphplot = reshape(sphplot, size(xp));
+    set(0, 'CurrentFigure', 1);
+    set(Hplot, 'CData', sphplot);
+    title( ['u at time ' num2str(t) ', kt= ' num2str(kt)] );
+    drawnow() ;
+    % writeVideo(writerObj, getframe(gcf)) ;     
+  end
+
+  if kt >= 8400 && mod(kt,50) == 0 && kt <= 9000
+      filename = ['results/GS_kt=', num2str(kt), '.mat'] ;
+      save(filename)
+  end
+
+  t = t + dt;
+  kt = kt + 1 ; 
+
+end
+
+
+% close(writerObj)
---
+++ examples_GBC/steklov_hemisphere.m
@@ -0,0 +1,150 @@
+%% Steklov problem on hemisphere 
+
+clear ;
+close all ; 
+
+
+%% Construct a grid in the embedding space
+
+dx = 0.05 ;  % grid spacing
+R = 1 ; %Radius of the circle
+
+pad = 5 ;
+x1d = ((-R-pad*dx):dx:(R+pad*dx))' ;
+y1d = x1d ;
+z1d = y1d ; 
+nx = length(x1d) ;
+ny = length(y1d) ;
+nz = length(z1d) ;
+[xx, yy, zz] = meshgrid(x1d,y1d,z1d) ;
+
+
+%% Find closest points on the surface
+
+cpf = @cpHemisphere ;
+
+[cpbarx, cpbary, cpbarz, dist, bdy] = cpbar_3d(xx, yy, zz, cpf, R) ;
+[cpx, cpy, cpz, ~, ~] = cpHemisphere(xx, yy, zz) ;
+
+
+%% Banding
+
+dim = 3;
+p = 1 ;      % Low interp degree (for plotting)
+q = 3 ;      % High interp degree (for computation)
+order = 2 ;  % Laplacian order
+bw = 1.0001*sqrt((dim-1)*((q+1)/2)^2 + ((order/2+(q+1)/2)^2));
+if (pad < bw)
+  warning('maybe insufficient padding');
+end
+band = find(abs(dist) <= bw*dx);
+outband = find(abs(dist) > bw*dx);
+
+% Last place we need the 3D array
+xx_band = xx(band) ;
+yy_band = yy(band) ;
+zz_band = zz(band) ;
+cpbarx_band = cpbarx(band) ;
+cpbary_band = cpbary(band) ;
+cpbarz_band = cpbarz(band) ; 
+dist_band = dist(band) ;
+bdy_band = bdy(band) ;
+
+cpx_band = cpx(band) ;
+cpy_band = cpy(band) ;
+cpz_band = cpz(band) ; 
+
+
+%% Construct an interpolation matrix for closest point
+
+% Interpolation matrice for modified closest point 
+Ebarqmat = interp3_matrix(x1d,y1d,z1d, cpbarx_band, cpbary_band, cpbarz_band, q, band);
+
+% Interpolation matrice for closest point 
+Eqmat = interp3_matrix(x1d,y1d,z1d, cpx_band, cpy_band, cpz_band, q, band);
+
+
+%% Create Laplacian matrix 
+
+Lmat = laplacian_3d_matrix(x1d,y1d,z1d, order, band, band);
+
+
+%% plotting grid
+
+[xp,yp,zp] = paramHemisphere(100, R) ;
+xp1 = xp(:) ;  yp1 = yp(:) ;  zp1 = zp(:) ;
+
+% % Eplot is a matrix which interpolates data onto the plotting grid
+Eplot = interp3_matrix(x1d,y1d,z1d, xp1,yp1,zp1, 3, band) ;
+
+
+%% Setting up linear system
+
+n = [0;0;-1] ;
+dvec = zeros(size(band)) ; 
+for k = 1:length(band)
+    if bdy_band(k) == 1      
+        xtmp = xx_band(k) ; 
+        ytmp = yy_band(k) ; 
+        ztmp = zz_band(k) ;
+        cpxtmp = cpx_band(k) ; 
+        cpytmp = cpy_band(k) ; 
+        cpztmp = cpz_band(k) ;   
+        cpbarxtmp = cpbarx_band(k) ; 
+        cpbarytmp = cpbary_band(k) ; 
+        cpbarztmp = cpbarz_band(k) ;  
+        w =[xtmp; ytmp; ztmp] - [cpxtmp; cpytmp; cpztmp] ;
+        n = [cpxtmp; cpytmp; cpztmp] - [cpbarxtmp; cpbarytmp; cpbarztmp] ;
+        n = n/norm(n) ;
+        if norm([cpx_band(k)-cpbarx_band(k); cpy_band(k)-cpbary_band(k); cpz_band(k)-cpbarz_band(k)],2) >= 1e-4
+            dvec(k) = 2*(w'*n) ;
+        end
+    end
+end
+
+N = length(band) ;
+Dmat = spdiags(dvec, 0, N, N) ;
+
+gamma = 2*dim/dx^2 ; % penalty parameter
+Imat = speye(size(Ebarqmat)) ;
+Amat = Eqmat*Lmat - gamma*(Imat - Ebarqmat) ;
+Bmat = -gamma*(Dmat*Eqmat) ;
+
+
+%% Compute Steklov spectrum 
+
+numrow = 5 ; 
+numcol = 6 ; 
+
+[V,D] = eigs(Amat, Bmat, numrow*numcol + 1, 0.1);
+
+D = diag(D);
+[Lambda,I] = sort(abs(D));
+disp(Lambda)
+
+
+figure(1) ; clf ;
+colormap('cool')
+
+tiledlayout(numrow, numcol)
+idxset = 2:numrow*numcol+1 ; 
+
+for i = 1:length(idxset)
+    idx = idxset(i) ;
+    lambda = Lambda(idx);
+    eigenvec = V(:,I(idx));
+    
+    uplot = Eplot*Eqmat*real(eigenvec);
+    uplot = reshape(uplot, size(xp));
+    nexttile
+    surf(xp, yp, zp, uplot);    
+    xlabel('x') ; ylabel('y') ; zlabel('z');
+    title(['ev = ' num2str(lambda)]);
+    axis equal ; axis off ;
+    shading interp
+end
+
+set(gcf, 'Position', [1           1        1512         773])
+
+
+
